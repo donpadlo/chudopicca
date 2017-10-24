6eb9c7c4597cf90e28e7f38bc66963c53b0c5697
donpadlo <donpadlo@gmail.com>
2017-10-24 13:58:16 +0300

    -  мелкие изменения

--- controller/client/themes/bootstrap/about.php
+++ controller/client/themes/bootstrap/about.php
@@ -4,7 +4,7 @@
 		<script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Ac40fe8a9055019cfa8c70f218699bf483670793af29287d2a7ccbfb1a6cdf39d&amp;width=100%25&amp;height=400&amp;lang=ru_RU&amp;scroll=true"></script>	
 	    </div>
 	    <div class="col-xs-12 col-sm-4 col-md-4 col-lg-2" style="padding-right: 0px;padding-left: 0px;">    
-		<div id="vk_widget"></div>
+		<div id="vk_widget"><div align=center><img src="/controller/client/themes/bootstrap/img/animpicca.gif"><br/><h2>Идет загрузка страницы..</h2></div></div>
 		<script type="text/javascript" src="//vk.com/js/api/openapi.js?116"></script>
 		<script>
 		    function VK_Widget_Init(){
@@ -14,7 +14,7 @@
 		    window.addEventListener('load', VK_Widget_Init, false);
 		    window.addEventListener('resize', VK_Widget_Init, false);
 		</script>		
-		<div id="vk_widget1"></div>
+		<div id="vk_widget1"><div align=center><img src="/controller/client/themes/bootstrap/img/animpicca.gif"><br/><h2>Идет загрузка страницы..</h2></div></div>
 		<script type="text/javascript" src="//vk.com/js/api/openapi.js?116"></script>
 		<script>
 		    function VK_Widget_Init(){
