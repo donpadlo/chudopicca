4e61e884289630f8474bc8c7e70ac12a43f9e70a
donpadlo <donpadlo@gmail.com>
2017-10-26 13:54:46 +0300

    - мелкие изменения

--- controller/client/themes/bootstrap/about.php
+++ controller/client/themes/bootstrap/about.php
@@ -4,8 +4,8 @@
 		 <h4>г. Вологда ул. Дальняя д.32</h4>
 		 <h3>Наш телефон</h3>
 		 <h4>70-17-70</h4>
-	    <div class="col-xs-12 col-sm-8 col-md-8 col-lg-10" style="padding-right: 0px;padding-left: 0px;">   		
-		<script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Ac40fe8a9055019cfa8c70f218699bf483670793af29287d2a7ccbfb1a6cdf39d&amp;width=100%25&amp;height=400&amp;lang=ru_RU&amp;scroll=true"></script>	
+	    <div class="col-xs-12 col-sm-8 col-md-8 col-lg-10" style="padding-right: 0px;padding-left: 0px;">   				
+		<script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Ac40fe8a9055019cfa8c70f218699bf483670793af29287d2a7ccbfb1a6cdf39d&amp;width=100%&amp;height=400&amp;lang=ru_RU&amp;scroll=true"></script>
 	    </div>
 	    <div class="col-xs-12 col-sm-4 col-md-4 col-lg-2" style="padding-right: 0px;padding-left: 0px;">    
 		<div id="vk_widget"><div align=center><img src="/controller/client/themes/bootstrap/img/animpicca.gif"><br/><h2>Идет загрузка страницы..</h2></div></div>
