5390013499e5d592dac13570b1227e91140f2fe6
donpadlo <donpadlo@gmail.com>
2017-10-31 15:52:19 +0300

    - мелкие изменения

--- controller/client/themes/bootstrap/footer.php
+++ controller/client/themes/bootstrap/footer.php
@@ -10,6 +10,22 @@ $time = $time_end - $time_start;
           <div class="container-fluid row-fluid">
 	      <div class="col-xs-12 col-md-12 col-sm-12" style="align:center;">
                     <p>&copy; <a href="http://грибовы.рф" target="_blank"> 2017-<?php echo date("Y");?></a>. Собрано за <?php echo "$time";?>сек.</p>
+		    <!-- Rating@Mail.ru counter -->
+<script type="text/javascript">
+var _tmr = window._tmr || (window._tmr = []);
+_tmr.push({id: "2938393", type: "pageView", start: (new Date()).getTime()});
+(function (d, w, id) {
+  if (d.getElementById(id)) return;
+  var ts = d.createElement("script"); ts.type = "text/javascript"; ts.async = true; ts.id = id;
+  ts.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//top-fwz1.mail.ru/js/code.js";
+  var f = function () {var s = d.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ts, s);};
+  if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); }
+})(document, window, "topmailru-code");
+</script><noscript><div>
+<img src="//top-fwz1.mail.ru/counter?id=2938393;js=na" style="border:0;position:absolute;left:-9999px;" alt="" />
+</div></noscript>
+<!-- //Rating@Mail.ru counter -->
+
                 </div>
               </div>
       </footer>
